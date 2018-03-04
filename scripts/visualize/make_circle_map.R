aef19f27074ac5cc2f220e531ee60eda14347d78
Jordan S Read <jread@usgs.gov>
2018-03-04 15:25:37 -0600

    clean pie map

--- scripts/visualize/make_circle_map.R
+++ scripts/visualize/make_circle_map.R
@@ -53,15 +53,18 @@ visualize.make_pie_map <- function(viz){
   circle_sp_transf <- sp::spTransform(circle_sp, sp::CRS(crs))
   map_data_usa <- sf::st_as_sf(maps::map('usa', fill=TRUE, plot = FALSE))
   map_data_usa <- sf::st_transform(map_data_usa, crs)
-  map_data_county <- sf::st_as_sf(maps::map('county', fill=TRUE, plot = FALSE))
-  map_data_county <- sf::st_transform(map_data_county, crs)
+  map_data_state <- sf::st_as_sf(maps::map('state', fill=TRUE, plot = FALSE)) %>% 
+    sf::st_transform(crs)
+  map_data_county <- sf::st_as_sf(maps::map('county', fill=TRUE, plot = FALSE)) %>% 
+    sf::st_transform(crs)
   
   
   png(viz[["location"]], width = 10, height = 6.0, res=450, units = 'in')
   
   par(mai=c(0,0,0,0), omi=c(0,0,0,0), xaxs = 'i', yaxs = 'i')
-  plot(sf::st_geometry(map_data_usa), col = "#f1f1f1", border = "white")
-  plot(sf::st_geometry(map_data_county), col = NA, border = "white", add=TRUE, lwd = 0.5)
+  plot(sf::st_geometry(map_data_usa), col = "grey88", border = NA)
+  plot(sf::st_geometry(map_data_county), col = NA, border = "grey93", add=TRUE, lwd = 0.5)
+  plot(sf::st_geometry(map_data_state), col = NA, border = "white", add=TRUE, lwd = 0.75)
   
   # start w/ irrigation, fill around the categories (make sure they add up!)
   # note: they don't add up because we aren't including all
@@ -69,14 +72,16 @@ visualize.make_pie_map <- function(viz){
   
   for (j in seq_len(length(circle_sp_transf))){
     # this is in a loop because it is throwaway code: 
+    orig_ang <- 0
     r <- circle_sp_transf$radius_total[j] *rad_multiplier
     c.x <- coordinates(circle_sp_transf)[j, ][['x']]
     c.y <- coordinates(circle_sp_transf)[j, ][['y']]
     for (cat in categories){
       cat_angle <- circle_sp_transf[[cat]][j] / circle_sp_transf[['total']][j]*2*pi
       if (cat == head(categories, 1L)){
-        # start the first category mirrorer relative to the top
+        # start the first category mirror relative to the top
         angle_from <- pi/2 - cat_angle/2
+        orig_ang <- angle_from
       } else {
         angle_from <- angle_to
       }
@@ -84,9 +89,18 @@ visualize.make_pie_map <- function(viz){
       if (!is.na(cat_angle) & cat_angle > 0.01){
         segments <- make_arc(c.x, c.y, r = r, angle_from, angle_to)
         polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
-                border = color_by_wu_type(cat)$outline, 
+                border = NA,
                 col = color_by_wu_type(cat)$fill, lwd=0.25)
+        lines(segments$x, segments$y, lwd=0.5, col = color_by_wu_type(cat)$outline)
       }
+    }
+    if (!is.na(r) & cat == tail(categories, 1L) & angle_to < 2*pi + orig_ang){
+      
+      segments <- make_arc(c.x, c.y, r = r, angle_to, 2*pi + orig_ang)
+      polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
+              border = NA,
+              col = color_by_wu_type('other')$fill, lwd=0.25)
+      lines(segments$x, segments$y, lwd=0.5, col = color_by_wu_type(cat)$outline)
     }
   }
 
@@ -110,10 +124,12 @@ color_by_wu_type <- function(wu_type) {
                         fill = rgb(46/255, 134/255, 171/255, 0.8)),
          "thermoelectric" = list(outline = rgb(201/255, 148/255, 3/255),
                                  fill = rgb(252/255, 186/255, 4/255, 0.8)),
-         "publicsupply" = list(outline = rgb(148/255, 40/255, 32/255),
-                               fill = rgb(186/255, 50/255, 40/255, 0.8)),
+         "publicsupply" = list(outline = rgb(36/255, 107/255, 136/255),
+                               fill = rgb(46/255, 134/255, 171/255, 0.8)),
          "irrigation" = list(outline = rgb(124/255, 157/255, 48/255),
                              fill = rgb(155/255, 197/255, 61/255, 0.8)),
-         "industrial" = list(outline = rgb(110/255, 90/255, 84/255),
+         "industrial" = list(outline = rgb(148/255, 40/255, 32/255),
+                             fill = rgb(186/255, 50/255, 40/255, 0.8)),
+         "other" = list(outline = rgb(110/255, 90/255, 84/255),
                              fill = rgb(138/255, 113/255, 106/255, 0.8)))
 }
